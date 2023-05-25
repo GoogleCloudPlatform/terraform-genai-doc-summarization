df605594f961ba9ef38d5d4880961eb39e2203ce
Nick Cain <nicholascain@google.com>
2023-05-25 21:51:09 +0000

    linting terraform/webhook/main.py and adding license file

--- terraform/webhook/main.py
+++ terraform/webhook/main.py
@@ -1,15 +1,30 @@
+# Copyright 2023 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import datetime
 import json
 import os
-import datetime
 import re
 
+from cloudevents.http import CloudEvent
+import functions_framework
 from google.auth import default
 import vertexai
 from vertexai.preview.language_models import TextGenerationModel
-import functions_framework
 
-_PROJECT_ID=os.environ['PROJECT_ID']
-_LOCATION=os.environ['LOCATION']
+_PROJECT_ID = os.environ['PROJECT_ID']
+_LOCATION = os.environ['LOCATION']
 _CREDENTIALS, _ = default(scopes=['https://www.googleapis.com/auth/cloud-platform'])
 
 _MOCK_TEXT = '''
@@ -51,21 +66,21 @@ _DEFAULT_PARAMETERS = {
 }
 
 
-def coerce_datetime_zulu(input_datetime):
+def coerce_datetime_zulu(input_datetime: str) -> datetime.datetime:
 
-  regex = re.compile(r"(.*)(Z$)")
-  regex_match = regex.search(input_datetime)
-  if regex_match:
-      assert input_datetime.startswith(regex_match.group(1))
-      assert input_datetime.endswith(regex_match.group(2))
-      return datetime.datetime.fromisoformat(f'{input_datetime[:-1]}+00:00')
-  raise RuntimeError(
-      'The input datetime is not in the expected format. '
-      'Please check the format of the input datetime. Expected "Z" at the end'
-  )
+    regex = re.compile(r"(.*)(Z$)")
+    regex_match = regex.search(input_datetime)
+    if regex_match:
+        assert input_datetime.startswith(regex_match.group(1))
+        assert input_datetime.endswith(regex_match.group(2))
+        return datetime.datetime.fromisoformat(f'{input_datetime[:-1]}+00:00')
+    raise RuntimeError(
+        'The input datetime is not in the expected format. '
+        'Please check the format of the input datetime. Expected "Z" at the end'
+    )
 
 
-def default_marshaller(o):
+def default_marshaller(o: object) -> str:
     if isinstance(o, (datetime.date, datetime.datetime)):
         return o.isoformat()
     return str(o)
@@ -81,44 +96,43 @@ def summarize_text(text: str, parameters: None | dict[str, int | float] = None) 
 
     final_parameters = _DEFAULT_PARAMETERS.copy()
     if parameters:
-      final_parameters.update(parameters)
+        final_parameters.update(parameters)
 
     model = TextGenerationModel.from_pretrained("text-bison@001")
     response = model.predict(
-      f'Provide a summary with about two sentences for the following article: {text}\n'
-      'Summary:'
-    , **final_parameters)
+        f'Provide a summary with about two sentences for the following article: {text}\n'
+        'Summary:',
+        **final_parameters,
+    )
     print(f"Response from Model: {response.text}")
 
     return response.text
 
 
 @functions_framework.cloud_event
-def entrypoint(request):
+def entrypoint(request: CloudEvent) -> dict[str, str]:
 
-  metadata = dict(
-    event_id = request["id"],
-    event_type = request["type"],
-    bucket = request.data["bucket"],
-    name = request.data["name"],
-    metageneration = request.data["metageneration"],
-    timeCreated = coerce_datetime_zulu(request.data["timeCreated"]),
-    updated = coerce_datetime_zulu(request.data["updated"]),
-  )
+    metadata = dict(
+        event_id=request["id"],
+        event_type=request["type"],
+        bucket=request.data["bucket"],
+        name=request.data["name"],
+        metageneration=request.data["metageneration"],
+        timeCreated=coerce_datetime_zulu(request.data["timeCreated"]),
+        updated=coerce_datetime_zulu(request.data["updated"]),
+    )
 
-  try:
-    response = {
-      'revision': os.environ['REVISION'],
-      'summary': summarize_text(_MOCK_TEXT),
-    }
-  except Exception as e:
-    response = {
-      'exception': str(e),
-    }
-  response['metadata'] = metadata
+    try:
+        response = {
+            'revision': os.environ['REVISION'],
+            'summary': summarize_text(_MOCK_TEXT),
+        }
+    except Exception as e:
+        response = {
+            'exception': str(e),
+        }
+    response['metadata'] = metadata
 
+    print(json.dumps({'response': response}, indent=2, sort_keys=True, default=default_marshaller))
 
-  
-  print(json.dumps({'response': response}, indent=2, sort_keys=True, default=default_marshaller))
-
-  return response
+    return response

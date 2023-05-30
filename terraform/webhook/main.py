84cd8a6c81130cfdf1b502dea3768c686a97fbff
Yvonne <liyvonne@google.com>
2023-05-30 13:56:56 -0700

    add tuning method

--- terraform/webhook/main.py
+++ terraform/webhook/main.py
@@ -2,6 +2,7 @@ import json
 import os
 import datetime
 import re
+from typing import Union
 
 from google.auth import default
 import vertexai
@@ -43,6 +44,24 @@ learning (Chordia, Subrahmanyam, and Tong 2014; McLean and Pontiff \
 2016; Martineau 2021).
 '''.strip()
 
+_MOCK_DATA = [
+  {'input_text': 'What has been found since the 2010s?',
+    'output_text': 'return predictability has become more elusive'},
+  {'input_text': 'What is the efficient-market hypothesis?',
+    'output_text': 'it is impossible to "beat the market" consistently on a risk-adjusted basis'},
+  {'input_text': 'What is the direct implication of the efficient-market hypothesis?',
+    'output_text': 'risk adjustment'},
+  {'input_text': 'What is the EMH formulated in terms of?',
+    'output_text': 'market anomalies'},
+  {'input_text': 'What is the result of research in financial economics since at least the 1990s?',
+    'output_text': 'Eugene Fama'},
+  {'input_text': 'Who is the idea that financial market returns are difficult to predict associated with?',
+    'output_text': 'modern risk-based theories of asset prices'},
+  {'input_text': 'What does the EMH provide the basic logic for?',
+    'output_text': 'Rosenberg, Reid, and Lanstein 1985; Campbell and Shiller 1988; Jegadeesh and Titman 1993'},
+  {'input_text': 'What are some examples of return predictors?',
+    'output_text': 'return predictability has become more elusive'}]
+
 _DEFAULT_PARAMETERS = {
     "temperature": .2,
     "max_output_tokens": 256,
@@ -70,6 +89,7 @@ def default_marshaller(o):
         return o.isoformat()
     return str(o)
 
+
 def generate_questions(text: str, parameters: None | dict[str, int | float] = None) -> str:
     """Generate sample questions with a Large Language Model"""
     vertexai.init(
@@ -89,6 +109,7 @@ def generate_questions(text: str, parameters: None | dict[str, int | float] = No
     print(f"Questions generated from Model: {response.text}")
 
     return response.text
+
 
 def question_answering(text: str, parameters: None | dict[str, int | float] = None) -> str:
     """Answer the extracted questions with a Large Language Model"""
@@ -122,6 +143,41 @@ def question_answering(text: str, parameters: None | dict[str, int | float] = No
       'Answer7:\n'
     , **final_parameters)
     print(f"Answers of extractive questions from Model: {response.text}")
+
+
+def tune_model(
+    project_id: str,
+    location: str,
+    training_data: Union[pd.DataFrame, str],
+    train_steps: int = 10,
+):
+  """Prompt-tune a new model, based on a prompt-response data.
+
+  "training_data" can be either the GCS URI of a file formatted in JSONL format
+  (for example: training_data=f'gs://{bucket}/{filename}.jsonl'), or a pandas
+  DataFrame. Each training example should be JSONL record with two keys, for
+  example:
+    {
+      "input_text": <input prompt>,
+      "output_text": <associated output>
+    },
+  or the pandas DataFame should contain two columns:
+    ['input_text', 'output_text']
+  with rows for each training example.
+
+  Args:
+    project_id: GCP Project ID, used to initialize aiplatform
+    location: GCP Region, used to initialize aiplatform
+    training_data: GCS URI of training file or pandas dataframe of training data
+    train_steps: Number of training steps to use when tuning the model.
+  """
+  vertexai.init(project=project_id, location=location)
+  model = TextGenerationModel.from_pretrained("google/text-bison-001")
+
+  model.tune_model(
+      training_data=training_data,
+      train_steps=train_steps,
+  )
 
 def summarize_text(text: str, parameters: None | dict[str, int | float] = None) -> str:
     """Summarization Example with a Large Language Model"""
@@ -163,6 +219,7 @@ def entrypoint(request):
       'revision': os.environ['REVISION'],
       'questions': generate_questions(_MOCK_TEXT),
       'answers': question_answering(_MOCK_TEXT),
+      'tuning': tune_model(_MOCK_DATA),
       'summary': summarize_text(_MOCK_TEXT),
     }
   except Exception as e:

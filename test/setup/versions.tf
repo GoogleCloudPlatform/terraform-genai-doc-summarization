d670bbfd1e80f088ac95587cce18c24af78ff244
David Cavazos <dcavazos@google.com>
2025-04-03 13:44:07 -0700

    downgrade to compatible google version

--- test/setup/versions.tf
+++ test/setup/versions.tf
@@ -19,11 +19,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 6.28"
+      version = "~> 5.45"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = "~> 6.28"
+      version = "~> 5.45"
     }
   }
 }

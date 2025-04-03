8137b61c533e27beefc331f6170041d0e2f1e84a
David Cavazos <dcavazos@google.com>
2025-04-03 13:36:07 -0700

    upgrade hashicorp/google

--- test/setup/versions.tf
+++ test/setup/versions.tf
@@ -19,11 +19,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 5.45"
+      version = "~> 6.28"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = "~> 5.45"
+      version = "~> 6.28"
     }
   }
 }
